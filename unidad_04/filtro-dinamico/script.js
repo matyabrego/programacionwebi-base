05373079a33d04be9f8d6144874976136e6c7374
damian spizzirri <damianspizzirri@gmail.com>
2023-10-18 18:02:52 -0300

    agrega paises

---
+++ unidad_04/filtro-dinamico/script.js
@@ -0,0 +1,10 @@
+const pais1 = { pais: "Argentina", capital: "Buenos Aires" }
+const pais2 = { pais: "Brasil", capital: "Brasilia" }
+const pais3 = { pais: "Ecuador", capital: "Quito" }
+const pais4 = { pais: "Peru", capital: "Lima" }
+const pais5 = { pais: "Francia", capital: "Paris" }
+const pais6 = { pais: "España", capital: "Madrid" }
+const pais7 = { pais: "Portugal", capital: "Lisboa" }
+const pais8 = { pais: "Suiza", capital: "Berna" }
+
+const PAISES = [pais1, pais2, pais3, pais4, pais5, pais6, pais7, pais8];
