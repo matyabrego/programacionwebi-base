09adc93d8cba09d9e6dec7d27b50399c91ba204e
Esteban David Alaníz <davidsgretsch@gmail.com>
2023-10-25 20:19:17 -0300

    Clase 25-octubre

--- unidad_04/filtro-dinamico/script.js
+++ unidad_04/filtro-dinamico/script.js
@@ -1,24 +1,42 @@
-const PAIS_1 = { nombre: "argentina", capital: "buenos aires" }
-const PAIS_2 = { nombre: "brasil", capital: "brasilia" }
-const PAIS_3 = { nombre: "ecuador", capital: "quito" }
-const PAIS_4 = { nombre: "peru", capital: "lima" }
-const PAIS_5 = { nombre: "francia", capital: "paris" }
-const PAIS_6 = { nombre: "españa", capital: "madrid" }
-const PAIS_7 = { nombre: "portugal", capital: "lisboa" }
-const PAIS_8 = { nombre: "suiza", capital: "berna" }
+const PAIS_1 = { nombre: "argentina", capital: "buenos aires" };
+const PAIS_2 = { nombre: "brasil", capital: "brasilia" };
+const PAIS_3 = { nombre: "ecuador", capital: "quito" };
+const PAIS_4 = { nombre: "peru", capital: "lima" };
+const PAIS_5 = { nombre: "francia", capital: "paris" };
+const PAIS_6 = { nombre: "españa", capital: "madrid" };
+const PAIS_7 = { nombre: "portugal", capital: "lisboa" };
+const PAIS_8 = { nombre: "suiza", capital: "berna" };
 
 const PAISES = [PAIS_1, PAIS_2, PAIS_3, PAIS_4, PAIS_5, PAIS_6, PAIS_7, PAIS_8];
 
 const nodoPalabraABuscar = document.querySelector("#palabra-a-buscar");
 
-nodoPalabraABuscar.addEventListener("keyup", e => {
+const handleListItemClick = (list) => {
+  list.forEach((listItem) => {
+    listItem.addEventListener("click", (event) => {
+			const target = event.target;
 
-    const palabraABuscar = nodoPalabraABuscar.value;
-    const nodoSection = document.getElementById("paises");
-    const paisesConLaPalabra = PAISES.filter(elemento => elemento.nombre.includes(palabraABuscar))
+			console.log(target.innerHTML);
+      console.log(target);
+    });
+  });
+};
 
-    nodoSection.innerHTML = "";
-    for(let pais of paisesConLaPalabra){
-        nodoSection.innerHTML += `<article> ${pais.nombre} </article>`;
-    }
-})
+nodoPalabraABuscar.addEventListener("keyup", (e) => {
+  const palabraABuscar = nodoPalabraABuscar.value;
+  const nodoSection = document.getElementById("paises");
+  const paisesConLaPalabra = PAISES.filter((elemento) =>
+    elemento.nombre.includes(palabraABuscar)
+  );
+
+  nodoSection.innerHTML = "";
+  for (let pais of paisesConLaPalabra) {
+    nodoSection.innerHTML += `<article class='country-item'> ${pais.nombre} </article>`;
+  }
+
+  const countryList = document.querySelectorAll(".country-item");
+  handleListItemClick(countryList);
+});
+
+const countryListLoaded = document.querySelectorAll(".country");
+handleListItemClick(countryListLoaded);
